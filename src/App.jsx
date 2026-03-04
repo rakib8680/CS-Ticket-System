e547e4357b56d092ddd19b40314b3099752a0e4e
rakib8680 <rakib.cst.1st@gmail.com>
2026-03-04 16:13:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1>This is App component</h1>
+      <button class="btn">Button</button>
+      <button class="btn w-64 rounded-full">Button</button>
+    </div>
+  );
+};
+
+export default App;
